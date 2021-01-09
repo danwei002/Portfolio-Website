56da14691905029bdf6837ab598312bbfb35b569
Daniel <danwei002@gmail.com>
2021-01-09 17:26:50 -0500

    Bootstrap support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/js/dist/util'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/dropdown'
