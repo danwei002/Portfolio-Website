78cda32cec5b3b886308dc2e75f2f78a8134b6f2
Daniel <danwei002@gmail.com>
2021-01-09 17:33:50 -0500

    Make component folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export default function App() {
+  return (
+    <div className="mt-2">
+      <p className="lead">
+        Hello my friends
+      </p>
+    </div>
+  )
 }
