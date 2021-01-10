caa8597492bfdcc515ec5f3c9f9b5b87b085d349
Daniel <danwei002@gmail.com>
2021-01-10 14:31:09 -0500

    Start about page

--- src/pages/components/Home/Home.js
+++ src/pages/components/Home/Home.js
@@ -3,13 +3,13 @@ import React, {useState, useEffect} from "react"
 import "./home.css"
 
 function Home() {
-    const choices= ["a Computer Enthusiast", "an avid Programmer", "a Software Engineering Student"]
+    const choices= ["a Computer Enthusiast", "an avid Programmer"]
     const [index, setIndex] = useState(0)
-    const [desc, setDesc] = useState(choices[Math.floor(Math.random() * choices.length)])
+    const [desc, setDesc] = useState(choices[0])
     
     useEffect(() => {
         const indexInterval = setInterval(() => {
-            setIndex(prevIndex => prevIndex === 2 ? 0: prevIndex + 1)
+            setIndex(prevIndex => prevIndex === 1 ? 0: prevIndex + 1)
         }, 3500)
 
         return () => clearInterval(indexInterval)
@@ -21,13 +21,20 @@ function Home() {
 
     return (
         <div>
-            <div className="container-fluid mt-5">
-                <div className="row">
-                    <div className="offset-lg-1 offset-md-1 col-lg-5 col-md-5 col-sm-12 col-12">
+            <div className="home container-fluid min-vh-100 mt-5 pt-5">
+                <div className="row pt-5">
+                    <div className="offset-lg-2 offset-md-2 col-lg-5 col-md-7 col-sm-12 col-12">
                         <h1 className="home-greeting">
-                            Hi! <br/>
-                            I'm Daniel Wei and I'm <br/><span key={desc} className="description-animation">{desc}</span>
+                            <span style={{fontWeight:"bold", lineHeight:"150%"}}>Hi!</span><br/>
+                            I'm Daniel and I'm <br/><span key={desc} className="description-animation">{desc}</span>
                         </h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="offset-lg-2 offset-md-2 col-lg-5 col-md-7 col-sm-12 col-12">
+                        <a className="mt-4 btn btn-primary py-2 px-3" href="#about">
+                            More About Me
+                        </a>
                     </div>
                 </div>
             </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,29 @@
 import React from "react"
-
-import Background from "./imgs/background.png"
+import { Helmet } from "react-helmet"
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Home/Home"
+import About from "./components/About/About"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
 
 export default function App() {
   return (
-    <div 
-      className="min-vh-100" 
-      style={{
-        backgroundColor:"#000214", 
-        backgroundImage: `url(${Background})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "right bottom",
-        backgroundSize: "contain"
-      }}
-    >
-      <Navbar/>
-      <Home/>
+    <div>
+      <Helmet>
+        <title>Daniel Wei</title>
+      </Helmet>
+
+      <div 
+        className="min-vh-100" 
+        style={{
+          backgroundColor:"#000214", 
+        }}
+      >
+        <Navbar/>
+        <Home/>
+        <About/>
+      </div>
     </div>
   )
 }
