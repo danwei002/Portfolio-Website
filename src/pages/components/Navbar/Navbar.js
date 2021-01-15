b515e10ac6d30adb772cd5d5fb28b454cba5f105
Daniel <danwei002@gmail.com>
2021-01-14 21:34:52 -0500

    Fix build problem?

--- src/pages/components/Navbar/Navbar.js
+++ src/pages/components/Navbar/Navbar.js
@@ -1,10 +1,5 @@
 import React from "react"
 
-import 'jquery/dist/jquery.min.js'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './navbar.css'
-
 function Navbar() {
     function handleClick(event) {
         event.preventDefault()
